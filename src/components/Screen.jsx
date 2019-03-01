909a3cad38af54606a38155289cf2aab0a7251ff
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-03-01 10:17:08 -0800

    Build app statically. Hard code masterButtonList. Render. Display.

---
+++ src/components/Screen.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Screen(props){
+  
+}
+
+
+
+
+Screen.propTypes = {
+  clicked: PropTypes.array,
+  pattern: PropTypes.func,
+  key: PropTypes.func,
+};
+
+export default Screen;
---
+++ src/components/Static.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { v4 } from 'uuid';
+
+class Static extends React.Component {
+  
+  constructor(props){
+    super(props);
+    this.state = {
+      masterButtonList: [
+        {
+          clicked: null,
+          pattern: 0,
+          id: v4()
+        },
+        {
+          clicked: null,
+          pattern: 0,
+          id: v4()
+        },
+        {
+          clicked: null,
+          pattern: 0,
+          id: v4()
+        },
+      ]
+    };
+  }
+  
+  render(){
+    return(
+      <div>
+      {this.state.masterButtonList.map((butt) =>
+        <div key={butt.id}>
+          <Screen
+            clicked={butt.clicked}
+            pattern={butt.pattern}
+            key={butt.id}
+          />
+          <button onClick={() => this.onHandleClick(butt.id)}>Click</button>
+          <hr/>
+        </div>
+      )}
+      </div>
+    );
+  }
+}
+  
+export default Static;
+  
+  
+  
+  
