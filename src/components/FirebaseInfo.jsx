6f46da73e07236f84904a9fdb37d90b2409430f9
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-03-08 08:29:12 -0800

    Create Firebase database for application, add credential path to .gitignore.

---
+++ src/components/FirebaseInfo.jsx
@@ -0,0 +1,14 @@
+
+<script src="https://www.gstatic.com/firebasejs/5.8.6/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCS6Ek4ye-CarQaafTPIW7_ZK_9bOWQzXw",
+    authDomain: "entertainment-security.firebaseapp.com",
+    databaseURL: "https://entertainment-security.firebaseio.com",
+    projectId: "entertainment-security",
+    storageBucket: "entertainment-security.appspot.com",
+    messagingSenderId: "270129005298"
+  };
+  firebase.initializeApp(config);
+</script>
