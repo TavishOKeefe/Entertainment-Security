1326f866a75172eec9d9a1ae745227ccb5506f73
Tavish OKeefe <okeefe.tavish@gmail.com>
2019-03-12 16:42:39 -0700

    Add Add images.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,27 +2,26 @@ import React from 'react';
 import Static from './Static';
 import HomePage from './HomePage';
 import { Switch, Route } from 'react-router-dom';
-// import art from '../assets/images/Art-8.jpg';
+import art from '../assets/images/Art-14.jpg';
 import Footer from './Footer';
 
 function App(){
-  //
-  // const backgroundImage = {
-  //   backgroundImage: 'url(' + art + ')',
-  //   backgrounSize: 'cover',
-  //   overflow: 'hidden',
-  //   backgroundPosition: '50% 0%',
-  //   width:'100%',
-  //   height: '100%',
-  //   backgroundSize: 'contain',
-  // };
+
+  const backgroundImage = {
+    backgroundImage: 'url(' + art + ')',
+    backgrounSize: 'cover',
+    overflow: 'hidden',
+    backgroundPosition: '50% 0%',
+    width:'100%',
+    height: '100%'
+  };
 
   const relieveHeadache = {
     backgroundColor: '#ecf0f1',
   };
 
   return(
-    <div style={relieveHeadache}>
+    <div style={backgroundImage}>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/static' component={Static} />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,25 +4,19 @@ import key from '../assets/images/Key-2.png';
 function Header(){
 
   const headerInfo = {
-    backgroundColor: '#ecf0f1',
     fontFamily: 'sans-serif',
     paddingTop: '5px',
     justifyContent: 'center',
     textAlign: 'center',
-  };
-
-  const title = {
     fontFamily: 'monospace, fantasy',
     fontSize: '20px',
-    paddingBottom: '5px'
+    paddingBottom: '5px',
+    color: 'red'
   };
 
   return(
     <div style={headerInfo}>
-      <img src={key}/>
-      <div style={title}>
-        <h1>E-S</h1>
-      </div>
+      <img src={key}/><h1>E-S</h1>
     </div>
   );
 }
